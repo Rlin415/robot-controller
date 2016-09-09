86ff605d6a5dcd16839e15b121a32f3e7c342ed1
Robert Lin <rlin415@gmail.com>
2016-09-08 17:18:27 -0700

    changes

--- server/controllers/api/robot.controller.js
+++ server/controllers/api/robot.controller.js
@@ -44,7 +44,7 @@ function picture(req, res) {
   console.log('hit picture api');
   exec('sudo pkill uv4l', (err, stdout, stderr) => {
     if (err) console.error(err);
-    exec('sudo raspistill -o -vf cam.jpg', (err, stdout, stderr) => {
+    exec('sudo raspistill -vf -o cam.jpg', (err, stdout, stderr) => {
       if (err) console.error(err);
       exec('sudo uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --server-option ' + '--port=9090' + ' --server-option ' + '--max-queued-connections=30' + ' --server-option ' + '--max-streams=25' + ' --server-option ' + '--max-threads=29' + ' --vf', (err, stdout, stdinn) => {
         if (err) console.error(err);
