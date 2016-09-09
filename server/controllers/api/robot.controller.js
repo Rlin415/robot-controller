a1065b31e963148767b7bb6ad55afb5b313865e8
Robert Lin <rlin415@gmail.com>
2016-09-08 17:23:28 -0700

    add delays

--- server/controllers/api/robot.controller.js
+++ server/controllers/api/robot.controller.js
@@ -44,16 +44,20 @@ function picture(req, res) {
   console.log('hit picture api');
   exec('sudo pkill uv4l', (err, stdout, stderr) => {
     if (err) console.error(err);
-    exec('sudo raspistill -vf -o cam.jpg', (err, stdout, stderr) => {
-      if (err) console.error(err);
-      exec('sudo uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --server-option ' + '--port=9090' + ' --server-option ' + '--max-queued-connections=30' + ' --server-option ' + '--max-streams=25' + ' --server-option ' + '--max-threads=29' + ' --vf', (err, stdout, stdinn) => {
+    setTimeout(() => {
+      exec('sudo raspistill -vf -o cam.jpg', (err, stdout, stderr) => {
         if (err) console.error(err);
-        exec('sudo python ../picturetext.py', (err, stdout, stderr) => {
-          if (err) console.error(err);
-          console.log('returned: ', stdout);
-          res.sendStatus(200);
-        })
+        setTimeout(() => {
+          exec('sudo uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --server-option ' + '--port=9090' + ' --server-option ' + '--max-queued-connections=30' + ' --server-option ' + '--max-streams=25' + ' --server-option ' + '--max-threads=29' + ' --vf', (err, stdout, stdinn) => {
+            if (err) console.error(err);
+            exec('sudo python ../picturetext.py', (err, stdout, stderr) => {
+              if (err) console.error(err);
+              console.log('returned: ', stdout);
+              res.sendStatus(200);
+            })
+          });
+        }, 5000);
       });
-    });
+    }, 1000);
   });
 }
