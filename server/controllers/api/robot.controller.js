69044da2214a3973530fb79c4f041ce8a3c8b29f
Robert Lin <rlin415@gmail.com>
2016-09-09 10:09:15 -0700

    more changes

--- server/controllers/api/robot.controller.js
+++ server/controllers/api/robot.controller.js
@@ -12,27 +12,27 @@ function forward(req, res) {
   console.log('hit forward api');
   exec('sudo python ../goforward.py', (err, stdout, stderr) => {
     if (err) console.error(err);
-    console.log('returned: ', stdout);
-    res.sendStatus(200);
-  })
+  });
+  console.log('send forward command to robot');
+  res.sendStatus(200);
 }
 
 function backward(req, res) {
   console.log('hit backward api');
   exec('sudo python ../gobackward.py', (err, stdout, stderr) => {
     if (err) console.error(err);
-    console.log('returned: ', stdout);
-    res.sendStatus(200);
-  })
+  });
+  console.log('send backward command to robot');
+  res.sendStatus(200);
 }
 
 function left(req, res) {
   console.log('hit left api');
   exec('sudo python ../turnleft.py', (err, stdout, stderr) => {
     if (err) console.error(err);
-    console.log('returned: ', stdout);
-    res.sendStatus(200);
-  })
+  });
+  console.log('send left command to robot');
+  res.sendStatus(200);
 }
 
 function right(req, res) {
